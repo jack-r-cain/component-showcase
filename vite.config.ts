bf0923582872d0e8c510fd4475691f364ca8135b
Jack Cain <122126626+jack-r-cain@users.noreply.github.com>
2025-12-13 20:13:04 -0500

    remove / in base route

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
-  base: '/component-showcase/',
+  base: '/component-showcase',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
